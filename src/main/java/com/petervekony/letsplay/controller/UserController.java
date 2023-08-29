ea9ac2f68c0e04fc9f323c7af276851ced12d550
Péter Vékony <peter.vekony@protonmail.com>
2023-08-29 11:59:53 +0200

    remove unnecessary imports

--- src/main/java/com/petervekony/letsplay/controller/UserController.java
+++ src/main/java/com/petervekony/letsplay/controller/UserController.java
@@ -2,12 +2,10 @@ package com.petervekony.letsplay.controller;
 
 import com.petervekony.letsplay.security.services.PrincipalData;
 import com.petervekony.letsplay.model.UserModel;
-import com.petervekony.letsplay.security.services.UserDetailsImpl;
 import com.petervekony.letsplay.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
